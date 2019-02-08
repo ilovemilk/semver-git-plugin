5e493f8f1823ae750bf5c8a58e77674a07b5fc32
Matthias Held <ilovemilk@wusa.io>
2019-02-08 22:08:31 +0100

    remove test

--- src/test/kotlin/io/wusa/SemverGitPluginTest.kt
+++ src/test/kotlin/io/wusa/SemverGitPluginTest.kt
@@ -4,9 +4,4 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class SemverGitPluginTest {
-    @Test
-    fun testMyLanguage() {
-        assertEquals("Kotlin", SemverGitPlugin().kotlinLanguage().name)
-        assertEquals(10, SemverGitPlugin().kotlinLanguage().hotness)
-    }
 }
