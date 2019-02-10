6cacaf98303c74d6365d35bbedb2579eb177d6f7
Matthias <ilovemilk@wusa.io>
2019-02-10 13:20:51 +0100

    fix regex, processbuilder args and conditions

--- src/main/kotlin/io/wusa/SemverGitPlugin.kt
+++ src/main/kotlin/io/wusa/SemverGitPlugin.kt
@@ -29,63 +29,57 @@ class SemverGitPlugin : Plugin<Project> {
         }
     }
 
-    private fun checkVersion(version: String): Version {
-        return parseVersion(version)
-    }
-
-    private fun parseVersion(version: String): Version {
-        val regex = "/^([0-9]+).([0-9]+).([0-9]+)(-([a-zA-Z0-9.-]+))?$/".toRegex()
-        val matchResults = regex.find(version)
-        val (major, minor, patch, suffix) = matchResults!!.destructured
+    fun parseVersion(version: String): Version {
+        val regex = """^([0-9]+)\.([0-9]+)\.([0-9]+)(-([a-zA-Z0-9.-]+))?$""".toRegex()
+        val matchResult = regex.find(version)
+        val (major, minor, patch, suffix) = matchResult!!.destructured
         return Version(major.toInt(), minor.toInt(), patch.toInt(), suffix)
     }
 
-    private fun getNextVersion(version: String, nextVersion: String, snapshotSuffix: String): Version {
+    fun bumpVersion(version: Version, nextVersion: String, snapshotSuffix: String): Version {
         when(nextVersion) {
             "major" -> {
-                val v = parseVersion(version)
-                if (v.suffix == null) {
-                    v.major += 1
-                    v.minor = 0
-                    v.patch = 0
+                if (version.suffix.isEmpty()) {
+                    version.major += 1
+                    version.minor = 0
+                    version.patch = 0
                 }
-                v.suffix = snapshotSuffix
-                return v
+                version.suffix = snapshotSuffix
+                return version
             }
             "minor" -> {
-                val v = parseVersion(version)
-                if (v.suffix == null) {
-                    v.minor += 1
-                    v.patch = 0
+                if (version.suffix.isEmpty()) {
+                    version.minor += 1
+                    version.patch = 0
                 }
-                v.suffix = snapshotSuffix
-                return v
+                version.suffix = snapshotSuffix
+                return version
             }
             "patch" -> {
-                val v = parseVersion(version)
-                if (v.suffix == null) {
-                    v.patch += 1
+                if (version.suffix.isEmpty()) {
+                    version.patch += 1
                 }
-                v.suffix = snapshotSuffix
-                return v
+                version.suffix = snapshotSuffix
+                return version
             }
             else -> {
-                return checkVersion(nextVersion)
+                return parseVersion(nextVersion)
             }
         }
     }
 
-    private fun getGitVersion(nextVersion: String, snapshotSuffix: String, dirtyMarker: String, gitArgs: String, projectDir: File): Version {
-        var process = ProcessBuilder("git describe --exact-match " + gitArgs)
+    fun getGitVersion(nextVersion: String, snapshotSuffix: String, dirtyMarker: String, gitArgs: String, projectDir: File): Version {
+        val splittedGitArgs = gitArgs.split(" ").toTypedArray()
+        var process = ProcessBuilder("git", "describe", "--exact-match", *splittedGitArgs)
                 .directory(projectDir)
                 .redirectError(ProcessBuilder.Redirect.INHERIT)
                 .start()
         process.waitFor()
         if (process.exitValue() == 0) {
             val output = process.inputStream.bufferedReader().use { it.readText() }
-            return checkVersion(output)
+            return parseVersion(output)
         }
-        process = ProcessBuilder("git describe --dirty --abbrev=7 " + gitArgs)
+        process = ProcessBuilder("git", "describe", "--dirty", "--abbrev=7", *splittedGitArgs)
                 .directory(projectDir)
                 .redirectError(ProcessBuilder.Redirect.INHERIT)
                 .start()
@@ -96,19 +90,19 @@ class SemverGitPlugin : Plugin<Project> {
             val dirty = describe.endsWith("-dirty")
             if (dirty)
                 describe = describe.substring(0, describe.length - 6)
-            val versionRegex = "/-[0-9]+-g[0-9a-f]+\$/".toRegex()
-            val (version) = versionRegex.find(describe)!!.destructured
-            val suffixRegex = "/-([0-9]+)-g([0-9a-f]+)\$/".toRegex()
+            val versionRegex = """-[0-9]+-g[0-9a-f]+$""".toRegex()
+            val version = describe.replace(versionRegex, "")
+            val suffixRegex = """-([0-9]+)-g([0-9a-f]+)$""".toRegex()
             val (count, sha) = suffixRegex.find(describe)!!.destructured
             var suffix = snapshotSuffix
             suffix = suffix.replace("<count>", count)
             suffix = suffix.replace("<sha>", sha)
             if (dirty)
-                suffix = suffix.replace("<dirty>", dirtyMarker);
+                suffix = suffix.replace("<dirty>", dirtyMarker)
             else
-                suffix = suffix.replace("<dirty>", dirtyMarker);
-            return getNextVersion(version, nextVersion, suffix)
+                suffix = suffix.replace("<dirty>", dirtyMarker)
+            return bumpVersion(parseVersion(version), nextVersion, suffix)
         }
-        return getNextVersion("0.0.0", nextVersion, "SNAPSHOT")
+        return bumpVersion(Version(0, 0, 0, ""), nextVersion, "SNAPSHOT")
     }
 }
