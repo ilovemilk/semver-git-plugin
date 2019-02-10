b7810b80fa37ea8cd5a2dd67a621055d52b52ea6
Matthias <ilovemilk@wusa.io>
2019-02-10 13:21:02 +0100

    add tests

--- src/test/kotlin/io/wusa/SemverGitPluginTest.kt
+++ src/test/kotlin/io/wusa/SemverGitPluginTest.kt
@@ -4,4 +4,33 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class SemverGitPluginTest {
+    @Test
+    fun parseVersionTest() {
+        assertEquals(SemverGitPlugin().parseVersion("1.1.1"), Version(1, 1, 1, ""))
+        assertEquals(SemverGitPlugin().parseVersion("1.0.0-5-g5242341"), Version(1, 0, 0, "-5-g5242341"))
+    }
+
+    @Test
+    fun bumpVersionMajorTest() {
+        val version = Version(1, 1, 1, "")
+        val nextVersion = "major"
+        assertEquals(SemverGitPlugin().bumpVersion(version, nextVersion, "SNAPSHOT"), Version(2, 0, 0, "SNAPSHOT"))
+        assertEquals(SemverGitPlugin().bumpVersion(version, nextVersion, ""), Version(2, 0, 0, ""))
+    }
+
+    @Test
+    fun bumpVersionMinorTest() {
+        val version = Version(1, 1, 1, "")
+        val nextVersion = "minor"
+        assertEquals(SemverGitPlugin().bumpVersion(version, nextVersion, "SNAPSHOT"), Version(1, 2, 0, "SNAPSHOT"))
+        assertEquals(SemverGitPlugin().bumpVersion(version, nextVersion, ""), Version(1, 2, 0, ""))
+    }
+
+    @Test
+    fun bumpVersionPatchTest() {
+        val version = Version(1, 1, 1, "")
+        val nextVersion = "patch"
+        assertEquals(SemverGitPlugin().bumpVersion(version, nextVersion, "SNAPSHOT"), Version(1, 1, 2, "SNAPSHOT"))
+        assertEquals(SemverGitPlugin().bumpVersion(version, nextVersion, ""), Version(1, 1, 2, ""))
+    }
 }
