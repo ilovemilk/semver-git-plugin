9b8f2ab352c4e96171c80af2cc96633e9f7b56a1
Matthias <ilovemilk@wusa.io>
2019-02-10 14:15:26 +0100

    add extension for configuration

---
+++ src/main/kotlin/io/wusa/SemverGitPluginExtension.kt
@@ -0,0 +1,18 @@
+package io.wusa
+
+open class SemverGitPluginExtension {
+    var nextVersion: String = DEFAULT_NEXT_VERSION
+
+    var snapshotSuffix: String = DEFAULT_SNAPSHOT_SUFFIX
+
+    var dirtyMarker: String = DEFAULT_DIRTY_MARKER
+
+    var gitDescribeArgs: String = DEFAULT_GIT_DESCRIBE_ARGS
+
+    companion object {
+        const val DEFAULT_NEXT_VERSION = "minor"
+        const val DEFAULT_SNAPSHOT_SUFFIX = "SNAPSHOT"
+        const val DEFAULT_DIRTY_MARKER = "-dirty"
+        const val DEFAULT_GIT_DESCRIBE_ARGS = "--match *[0-9].[0-9]*.[0-9]*"
+    }
+}
