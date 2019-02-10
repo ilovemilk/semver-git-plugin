670e581139f9f18295c10e9161eaf38f909aa76b
Matthias <ilovemilk@wusa.io>
2019-02-10 16:28:07 +0100

    add jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `maven-publish`
     `java-gradle-plugin`
+    `jacoco`
     kotlin("jvm") version "1.3.21"
 }
 
@@ -8,12 +9,24 @@ group = "io.wusa"
 version = "0.0.1-SNAPSHOT"
 
 dependencies {
-    implementation(kotlin("stdlib"))
+    implementation(kotlin("stdlib-jdk8"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.0")
 }
 
 repositories {
     mavenCentral()
+}
+
+tasks.named<JacocoReport>("jacocoTestReport").configure {
+    reports.xml.isEnabled = true
+    reports.html.isEnabled = true
+    dependsOn(tasks.named("test"))
+}
+
+tasks.getting(Test::class) {
+    useJUnitPlatform()
+    dependsOn(tasks.named("cleanTest"))
 }
 
 gradlePlugin {
