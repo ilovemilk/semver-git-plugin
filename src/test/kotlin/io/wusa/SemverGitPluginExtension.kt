73d20e1155ccff2f3a19bb1202411428e75715fd
Matthias Held <ilovemilk@wusa.io>
2019-02-11 22:04:47 +0100

    add tests for extension

---
+++ src/test/kotlin/io/wusa/SemverGitPluginExtension.kt
@@ -0,0 +1,16 @@
+package io.wusa
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SemverGitPluginExtensionTest {
+
+    @Test
+    fun testExtensionDefaults() {
+        val extension = SemverGitPluginExtension()
+        assertEquals(extension.nextVersion, "minor")
+        assertEquals(extension.snapshotSuffix, "SNAPSHOT")
+        assertEquals(extension.dirtyMarker, "-dirty")
+        assertEquals(extension.gitDescribeArgs, "--match *[0-9].[0-9]*.[0-9]*")
+    }
+}
