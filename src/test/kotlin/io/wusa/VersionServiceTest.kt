56f0d9a5b85b1b2d859f25cf6d9b658ba5eaa60d
Matthias Held <ilovemilk@wusa.io>
2019-04-18 22:07:25 +0200

    add first part of regex

--- src/test/kotlin/io/wusa/VersionServiceTest.kt
+++ src/test/kotlin/io/wusa/VersionServiceTest.kt
@@ -7,10 +7,10 @@ import java.lang.IllegalArgumentException
 class VersionServiceTest {
     @Test
     fun `parse version`() {
-        Assertions.assertEquals(VersionService.parseVersion("1.1.1"), Version(1, 1, 1, null))
-        Assertions.assertEquals(VersionService.parseVersion("1.0.0-5-g5242341-dirty"), Version(1, 0, 0, Suffix(5, "5242341", true)))
-        Assertions.assertEquals(VersionService.parseVersion("1.0.0-5-g5242341"), Version(1, 0, 0, Suffix(5, "5242341", false)))
-        Assertions.assertEquals(VersionService.parseVersion("5000.1.1000000-5-g5242341-dirty"), Version(5000, 1, 1000000, Suffix(5, "5242341", true)))
+        Assertions.assertEquals(VersionService.parseVersion("1.1.1"), Version(1, 1, 1, "", "", null))
+        Assertions.assertEquals(VersionService.parseVersion("1.0.0-5-g5242341-dirty"), Version(1, 0, 0, "", "", Suffix(5, "5242341", true)))
+        Assertions.assertEquals(VersionService.parseVersion("1.0.0-5-g5242341"), Version(1, 0, 0, "", "", Suffix(5, "5242341", false)))
+        Assertions.assertEquals(VersionService.parseVersion("5000.1.1000000-5-g5242341-dirty"), Version(5000, 1, 1000000, "", "", Suffix(5, "5242341", true)))
         Assertions.assertThrows(IllegalArgumentException::class.java) {
             VersionService.parseVersion("1.1.1 ")
         }
@@ -63,39 +63,39 @@ class VersionServiceTest {
 
     @Test
     fun `bump major version`() {
-        val version = Version(1, 1, 1, null)
+        val version = Version(1, 1, 1, "", "", null)
         val nextVersion = "major"
-        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(2, 0, 0, null))
-        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(3, 0, 0, null))
+        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(2, 0, 0, "", "", null))
+        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(3, 0, 0, "", "", null))
     }
 
     @Test
     fun `bump minor version`() {
-        val version = Version(1, 1, 1, null)
+        val version = Version(1, 1, 1, "", "", null)
         val nextVersion = "minor"
-        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(1, 2, 0, null))
-        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(1, 3, 0, null))
+        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(1, 2, 0, "", "", null))
+        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(1, 3, 0, "", "", null))
     }
 
     @Test
     fun `bump patch version`() {
-        val version = Version(1, 1, 1, null)
+        val version = Version(1, 1, 1, "", "", null)
         val nextVersion = "patch"
-        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(1, 1, 2, null))
-        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(1, 1, 3, null))
+        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(1, 1, 2, "", "", null))
+        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(1, 1, 3, "", "", null))
     }
 
     @Test
     fun testBumpVersionDefault() {
-        val version = Version(1, 1, 1, null)
+        val version = Version(1, 1, 1, "", "", null)
         val nextVersion = ""
-        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(1, 1, 1, null))
+        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(1, 1, 1, "", "", null))
     }
 
     @Test
     fun `don't bump version`() {
-        val version = Version(1, 1, 1, null)
+        val version = Version(1, 1, 1, "", "", null)
         val nextVersion = "none"
-        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(1, 1, 1, null))
+        Assertions.assertEquals(VersionService.bumpVersion(version, nextVersion), Version(1, 1, 1, "", "",null))
     }
 }
