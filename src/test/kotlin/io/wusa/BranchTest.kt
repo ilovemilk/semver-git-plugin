f50ef505e3b9c7bdcacc845fca0958803563b1f0
Matthias <ilovemilk@wusa.io>
2019-04-19 18:56:05 +0200

    fix tests

--- src/test/kotlin/io/wusa/BranchTest.kt
+++ src/test/kotlin/io/wusa/BranchTest.kt
@@ -2,14 +2,23 @@ package io.wusa
 
 import io.mockk.every
 import io.mockk.mockkObject
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import io.mockk.unmockkObject
+import org.junit.jupiter.api.*
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class BranchTest {
+    @BeforeEach
+    internal fun setUp() {
+        mockkObject(GitService.Companion)
+    }
+
+    @AfterEach
+    internal fun cleanUp() {
+        unmockkObject(GitService.Companion)
+    }
 
     @Test
     fun `group of master should be master`() {
-        mockkObject(GitService.Companion)
         val branch = Branch(createTempDir())
         every { GitService.currentBranch(projectDir = any()) } returns "master"
         Assertions.assertEquals(branch.group, "master")
@@ -17,7 +26,6 @@ class BranchTest {
 
     @Test
     fun `group of master should be develop`() {
-        mockkObject(GitService.Companion)
         val branch = Branch(createTempDir())
         every { GitService.currentBranch(projectDir = any()) } returns "develop"
         Assertions.assertEquals(branch.group, "develop")
@@ -25,7 +33,6 @@ class BranchTest {
 
     @Test
     fun `group of feature-test should be test`() {
-        mockkObject(GitService.Companion)
         val branch = Branch(createTempDir())
         every { GitService.currentBranch(projectDir = any()) } returns "feature/test"
         Assertions.assertEquals(branch.group, "feature")
@@ -33,7 +40,6 @@ class BranchTest {
 
     @Test
     fun `group of feature-test-test should be test`() {
-        mockkObject(GitService.Companion)
         val branch = Branch(createTempDir())
         every { GitService.currentBranch(projectDir = any()) } returns "feature/test/test"
         Assertions.assertEquals(branch.group, "feature")
@@ -41,7 +47,6 @@ class BranchTest {
 
     @Test
     fun `id of feature-test should be feature-test`() {
-        mockkObject(GitService.Companion)
         val branch = Branch(createTempDir())
         every { GitService.currentBranch(projectDir = any()) } returns "feature/test"
         Assertions.assertEquals(branch.id, "feature-test")
@@ -49,7 +54,6 @@ class BranchTest {
 
     @Test
     fun `id of feature-test_a! should be feature-test_a!`() {
-        mockkObject(GitService.Companion)
         val branch = Branch(createTempDir())
         every { GitService.currentBranch(projectDir = any()) } returns "feature/test_a!"
         Assertions.assertEquals(branch.id, "feature-test_a!")
@@ -57,7 +61,6 @@ class BranchTest {
 
     @Test
     fun `id of feature-special-test should be feature-special-test`() {
-        mockkObject(GitService.Companion)
         val branch = Branch(createTempDir())
         every { GitService.currentBranch(projectDir = any()) } returns "feature/special-test"
         Assertions.assertEquals(branch.id, "feature-special-test")
